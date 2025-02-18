24447e8490a9f220122eedb8f39942c7cd17222b
Jeeva <Jeeva@DESKTOP-SRUHC7J>
2025-02-18 20:42:47 +0800

    Updated changes

--- GICApplication/Program.cs
+++ GICApplication/Program.cs
@@ -17,7 +17,9 @@ namespace GICApplication
             do
             {
                 // options
+                Console.WriteLine("\n=======================================================");
                 Console.WriteLine("\nWelcome to Awesome GIC Bank! What would you like to do?");
+                Console.WriteLine("\n======================================================="); 
                 Console.WriteLine("[T] Input transactions");
                 Console.WriteLine("[I] Define interest rules");
                 Console.WriteLine("[P] Print statement");
@@ -25,26 +27,35 @@ namespace GICApplication
 
                 // get user's input value
                 Console.Write("Enter your choice: ");
-                char input = char.Parse(Console.ReadLine().ToUpper());
+                string input = Console.ReadLine()?.Trim().ToUpper();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    continue;
+                }
 
                 // exeucte the method based on user's input
-                switch (char.ToUpper(input))
+                switch (input)
                 {
-                    case 'T':
-                        Console.Write("Please enter transaction details in <Date> <AccountNo(Starts with AC)> <Type> <Amount> format \r\n(or enter blank to go back to main menu) :");
+                    case "T":
+                        Console.Write("Please enter transaction details in <Date> <AccountNo> <Type> <Amount> format");
+                        Console.Write("\r\n(or enter blank to go back to main menu) :");
                         transactionService.AddTransactions(Console.ReadLine().ToUpper());
                         Console.WriteLine("Transaction has been added successfully.");
                         break;
-                    case 'I':
-                        Console.Write("\nPlease enter interest rules details in <Date> <RuleId> <Rate in %> format \r\n(or enter blank to go back to main menu):");
+                    case "I":
+                        Console.Write("\nPlease enter interest rules details in <Date> <RuleId> <Rate in %> format");
+                        Console.Write("\r\n(or enter blank to go back to main menu) :");
                         transactionService.AddInterestRules(Console.ReadLine().ToUpper());
                         Console.WriteLine("Interest Rule has been added successfully.");
                         break;
-                    case 'P':
-                        Console.Write("\nPlease enter account and month to generate the statement <Account(Starts with AC)> <Year><Month> \r\n(or enter blank to go back to main menu) :");
+                    case "P":
+                        Console.Write("\nPlease enter account and month to generate the statement <Account> <Year><Month>");
+                        Console.Write("\r\n(or enter blank to go back to main menu) :");
                         transactionService.PrintStatement(Console.ReadLine().ToUpper());
                         break;
-                    case 'Q':
+                    case "Q":
                         Console.WriteLine("Thank you for using AwesomeGIC Bank. Have a nice day!");
                         exit = true;
                         break;
